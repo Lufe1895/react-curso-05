4f75604f658686af5e17e9ca8434c3535a30987f
Luis Fernando Antonio Toral <luislecompt@gmail.com>
2021-09-20 17:55:36 -0500

    Comienzo de las pruebas unitarias

---
+++ src/tests/components/GifGridItems.test.js
@@ -0,0 +1,30 @@
+import { shallow } from 'enzyme';
+import GifGridItem from '../../components/GifGridItem';
+
+describe('Pruebas en <GifGridItem/>', () => {
+    const title = "Título";
+    const url = "https://www.facebook.com";
+    const wrapper = shallow(<GifGridItem title={ title } url={ url } />);
+
+    test('Debe de mostrar el componente correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe tener un párrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('debe tener la imagen igual al url', () => {
+        const img = wrapper.find('img');
+        expect(img.props().src).toBe(url);
+        expect(img.props().alt).toBe(title);
+    })
+    
+    test('debe tener la animación', () => {
+        const div = wrapper.find('div');
+        const className = div.props().className;
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    })
+    
+})
